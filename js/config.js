3291a2cc6e628507222d43424c5cd2cf344fb473
urdailymotion <104983872+urdailymotion@users.noreply.github.com>
2026-07-27 03:54:13 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
     appName: 'Inventory OGDC',
 
     // Gunakan Project URL, bukan URL /rest/v1/
-    supabaseUrl: 'https://jdthhrqokvaflenuecsi.supabase.co',
+    supabaseUrl: 'https://jdthhrqokvaflenuecsi.supabase.co/rest/v1/',
 
     // Tempel Publishable Key terbaru dari Supabase
     supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkdGhocnFva3ZhZmxlbnVlY3NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MTI5OTEsImV4cCI6MjEwMDQ4ODk5MX0.I_T52YUmUpsIwM3B_MHx3J6_mZtXX5VvkKbeKBib3Yk',
